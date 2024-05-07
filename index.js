b3e15b008463dd5f3fd503bd6ed0ec283ba57bbf
ChristopherTsh <156902121+ChristopherTsh@users.noreply.github.com>
2024-05-07 14:05:42 +0200

    using functions for calculations and if statement for error

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ const fuel = 5000; // remaining fuel (kg)
 const fuelBurnRate = 0.5; // fuel burn rate (kg/s)
 
 
-const newDistance = distance + velocity //36 000 000 calculates new distance
-const remainingFuel = fuel-(fuelBurnRate*time)  //1 800 calculates remaining fuel
+const newDistance = calcNewDistance(distance , velocity) //36 000 000 calculates new distance
+const remainingFuel = calcNewRemainingFuel(fuel,fuelBurnRate,time)  //1 800 calculates remaining fuel
 const velocityBasedOnAcceleration = calcNewVel(velocity, acceleration, time) //calculates new velocity based on acceleration
 
 // Pick up an error with how the function below is called and make it robust to such errors
@@ -24,15 +24,28 @@ function calcNewVel(velocity, acceleration, time)  {
     throw new Error('only number are allowed');
   }
   return velocity + (acceleration*time)*(time/1000)
-} 
+}
+function calcNewDistance(distance , velocity){
+  if (typeof distance!== 'number' ||velocity !== 'number'){
+    throw new Error('only number are allowed');
+  }
+  return distance + velocity
+}
+function calcNewRemainingFuel(fuel,fuelBurnRate,time){
+  if (typeof fuel !== 'number' ||fuelBurnRate !== 'number'||time !== 'number'){
+    throw new Error('only number are allowed')
+}
+return fuel-(fuelBurnRate*time)
+}
 try{
+console.log(`Corrected New Distance: ${newDistance} km`);
+console.log(`Corrected Remaining Fuel: ${remainingFuel} kg`);
 console.log(`Corrected New Velocity: ${velocityBasedOnAcceleration} km/h`);
 } catch(error){
   console.error('Error:',error.message)
 }
 
-console.log(`Corrected New Distance: ${newDistance} km`);
-console.log(`Corrected Remaining Fuel: ${remainingFuel} kg`);
+
 
 
 
